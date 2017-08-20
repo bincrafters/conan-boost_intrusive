1d4e99c0ef7fc69b7fcabc24d2d16eb60f4ed660
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:49 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostIntrusiveConan(ConanFile):
     name = "Boost.Intrusive"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-intrusive"
     source_url = "https://github.com/boostorg/intrusive"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -19,7 +20,7 @@ class BoostIntrusiveConan(ConanFile):
 
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
