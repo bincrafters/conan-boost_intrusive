538e74fbbb24d5c03635bf3c61d50b1a8a7ce0f4
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:23:37 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostIntrusiveConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-intrusive"
-    source_url = "https://github.com/boostorg/intrusive"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["intrusive"]
@@ -19,9 +18,12 @@ class BoostIntrusiveConan(ConanFile):
                       #assert1 config0 core2 functional5 move3 static_assert1
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
