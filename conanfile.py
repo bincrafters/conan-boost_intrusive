3b09b27ce580fdee5fe332568e8fb31d1d699fae
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:44 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostIntrusiveConan(base.BoostBaseConan):
     name = "boost_intrusive"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_intrusive"
     lib_short_names = ["intrusive"]
     header_only_libs = ["intrusive"]
