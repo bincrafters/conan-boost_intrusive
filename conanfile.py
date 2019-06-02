b225cb374be3bbb579502ab8c3a89b964da0a3d3
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,19 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostIntrusiveConan(base.BoostBaseConan):
     name = "boost_intrusive"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_intrusive"
-    lib_short_names = ["intrusive"]
-    header_only_libs = ["intrusive"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_container_hash",
-        "boost_core",
-        "boost_move",
-        "boost_static_assert"
-    ]
+    version = "1.70.0"
